b0ca4112ad6fccd5ee4b9c7e8c3e1ae3d84eaea7
Alexander Vovnenko <vovnenko_ne@mail.ru>
2016-08-04 00:34:41 +0300

    add path tracer

---
+++ Assets/Scripts/MousePathTracer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Assets.Helpers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class MousePathTracer : MonoBehaviour
+    {
+        [SerializeField] 
+        private Image dotPrefab;
+        [SerializeField]
+        private int valuesCount = 150;
+        
+        private Queue<Image> dots;
+
+
+        private void Awake()
+        {
+            dots = new Queue<Image>(valuesCount);
+            for (int i = 0; i < valuesCount; i++)
+            {
+                Image image = PrefabHelper.Intantiate(dotPrefab, gameObject);
+                image.color = Color.clear;
+                dots.Enqueue(image);
+            }
+        }
+
+        private void Update()
+        {
+            if (!Input.GetMouseButton(0)) return;
+            Image image = dots.Dequeue();
+            image.color = Color.red;
+            image.transform.position = Input.mousePosition;
+            dots.Enqueue(image);
+        }
+
+    }
+}
